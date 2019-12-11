a6890b253c94ad328145df532110cc88570fc8a9
ltq <ltq526314589@163.com>
2019-12-11 17:02:23 +0800

    策略模式

--- src/main/java/com/allinmd/demo/IStudentPredicate.java
+++ src/main/java/com/allinmd/demo/IStudentPredicate.java
@@ -3,6 +3,10 @@ package com.allinmd.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 策略模式
+ * @author liutieqiang
+ */
 public interface IStudentPredicate {
     boolean test(Student stu);
 
